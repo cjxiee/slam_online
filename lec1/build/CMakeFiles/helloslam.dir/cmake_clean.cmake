e97becff0eca2a96a292f93f34d53b52f086a990
cjxiee <xxshijie@seas.upenn.edu>
2022-09-26 02:07:23 +0800

    env test

---
+++ lec1/build/CMakeFiles/helloslam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloslam.dir/main.cpp.o"
+  "helloslam"
+  "helloslam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
